ae60ead694cb849f0ed9bb17c3c906f05d40182e
candymate <parkjuny@postech.ac.kr>
2019-11-11 19:26:21 +0900

    Added backbone of plugin (Pair : JeongWoo Kim)

---
+++ project/src/main/java/com/SoftwareMatrix/MetricsResultWindow.java
@@ -0,0 +1,32 @@
+package com.SoftwareMatrix;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.components.JBScrollPane;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import javax.swing.tree.TreeModel;
+import java.awt.*;
+import java.util.Calendar;
+
+public class MetricsResultWindow {
+    /* Declare private fields here */
+
+    /**
+     * Constructor of tool window
+     */
+    public MetricsResultWindow(ToolWindow toolWindow) {
+        // constructor
+    }
+
+    /**
+     * Returns content of this tool window
+     * @return whole content of tool window
+     */
+    public JPanel getContent() {
+        return null;
+    }
+}
