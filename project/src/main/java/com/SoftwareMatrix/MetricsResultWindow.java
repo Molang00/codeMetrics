6ab012d8f27af70b3bfd84c8116c99cca9de3792
molang <bskim00@postech.ac.kr>
2019-11-13 19:53:30 +0900

    Made table showing scores and colors according to result (Pair: Seungjoon Lee)

--- project/src/main/java/com/SoftwareMatrix/MetricsResultWindow.java
+++ project/src/main/java/com/SoftwareMatrix/MetricsResultWindow.java
@@ -10,23 +10,29 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.tree.TreeModel;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
 import java.awt.*;
 import java.util.Calendar;
+import java.awt.image.BufferedImage;
 
 public class MetricsResultWindow {
     /* Declare private fields here */
     JPanel myToolWindowContent;
     JTable tableStructure;
     JScrollPane tableContent;
-    Integer OOscore, MIscore;
+    Integer MIscore, OOscore;
 
     /**
      * Constructor of tool window
      */
     public MetricsResultWindow(ToolWindow toolWindow) {
-        String header[] = { "\\", "score" };
-        String body[][] = { { "MI", "" }, { "OO", "" } };
+        String header[] = { "", "score", "graph" };
+        String body[][] = { { "MI", "", "" }, { "OO", "", "" } };
         DefaultTableModel model = new DefaultTableModel(body, header) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -40,6 +46,9 @@ public class MetricsResultWindow {
         settingAllStatus();
         tableStructure.setValueAt(MIscore, 0, 1);
         tableStructure.setValueAt(OOscore, 1, 1);
+        TableColumnModel tcm = tableStructure.getColumnModel();
+        TableColumn tm = tcm.getColumn(2);
+        tm.setCellRenderer(new ColoredTableCellRenderer());
     }
 
     public Integer getOOsocre() {
@@ -60,7 +69,7 @@ public class MetricsResultWindow {
 
     public void settingAllStatus() {
         setMIscore(86);
-        setOOscore(70);
+        setOOscore(46);
     }
 
     /**
@@ -70,5 +79,29 @@ public class MetricsResultWindow {
      */
     public JScrollPane getContent() {
         return tableContent;
+    }
+
+    class ColoredTableCellRenderer extends DefaultTableCellRenderer {
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focused,
+                int row, int column) {
+            setEnabled(table == null || table.isEnabled()); // see question above
+            int r, g, b = 0;
+
+            if (row == 0) {
+                r = (int) ((100 - MIscore) * 2.55);
+                g = (int) ((MIscore) * 2.55);
+                Color c = new Color(r, g, b);
+                setBackground(c);
+            } else {
+                r = (int) ((100 - OOscore) * 2.55);
+                g = (int) ((OOscore) * 2.55);
+                Color c = new Color(r, g, b);
+                setBackground(c);
+            }
+
+            super.getTableCellRendererComponent(table, value, selected, focused, row, column);
+
+            return this;
+        }
     }
 }
