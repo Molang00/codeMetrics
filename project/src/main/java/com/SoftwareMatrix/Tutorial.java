b2960ba84e48e801b65e1e981508c1fa827ac5d3
jjpark17 <jjpark17@postech.ac.kr>
2019-11-19 21:35:22 +0900

    Find solution for reading file in resources folder(Spent 1 hour)

--- project/src/main/java/com/SoftwareMatrix/Tutorial.java
+++ project/src/main/java/com/SoftwareMatrix/Tutorial.java
@@ -1,7 +1,7 @@
 package com.SoftwareMatrix;
 
-import android.os.SystemPropertiesProto;
 import com.intellij.ui.treeStructure.Tree;
+import org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -19,21 +19,21 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 
 public class Tutorial {
     private JTree tree;
     private DefaultMutableTreeNode root;
-    private StringProducer tutorial_producer;
 
     private static final Icon folderIcon = MetalIconFactory.getTreeFolderIcon();
     private static final Icon leafIcon = MetalIconFactory.getFileChooserDetailViewIcon();
 
     public Tutorial() {
         root = new DefaultMutableTreeNode("Tutorial");
-        tutorial_producer = new StringProducer();
     }
 
     private DefaultMutableTreeNode addNode(String nodeName, DefaultMutableTreeNode parent){
@@ -43,7 +43,7 @@ public class Tutorial {
     }
 
     public JTree getResult() {
-        addNode("Project_Description", root);
+        addNode("Project Description", root);
         DefaultMutableTreeNode metrics = addNode("Metrics", root);
         addNode("Maintainability_Index", metrics);
         addNode("Halstead_Metrics", metrics);
@@ -56,12 +56,50 @@ public class Tutorial {
 
         tree = new Tree(root);
 
+
         DefaultTreeCellRenderer cr = new DefaultTreeCellRenderer();
         cr.setOpenIcon(folderIcon);
         cr.setClosedIcon(folderIcon);
         cr.setLeafIcon(leafIcon);
 
         tree.setCellRenderer(cr);
+
+        InputStream is = Tutorial.class.getResourceAsStream("string.xml");
+        System.out.println(is);
+        assert is != null;
+
+        InputStream iss = this.getClass().getResourceAsStream("string.xml");
+        System.out.println(iss);
+        assert iss != null;
+
+//        try {
+//            String result = IOUtils.toString(is, StandardCharsets.UTF_8);
+//            System.out.println(result);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            String result = IOUtils.toString(iss, StandardCharsets.UTF_8);
+//            System.out.println(result);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        System.out.println(Tutorial.class.getResource("string.xml").getPath());
+
+//        this.getClass()
+
+//        System.out.println(Tutorial.class);
+//        System.out.println(Tutorial.class.getResource("string.xml"));
+//        System.out.println(Tutorial.class.getResourceAsStream("string.xml"));
+////        System.out.println(Tutorial.class.getResource("main/resources/string.xml").getFile());
+//        System.out.println(Thread.currentThread().getContextClassLoader().getResourceAsStream("string.xml"));
+//
+//        System.out.println(getClass().getClassLoader());
+//        System.out.println(getClass().getClassLoader().getResource("string.xml"));
+//        System.out.println(getClass().getClassLoader().getResource("string.xml").getFile());
 
         tree.addMouseListener(new MouseAdapter() {
             @Override
@@ -70,9 +108,30 @@ public class Tutorial {
                     JTree tt = (JTree) (e.getSource());
                     DefaultMutableTreeNode tn = (DefaultMutableTreeNode) tt.getLastSelectedPathComponent();
                     if (tn.isLeaf()) {
-                        String url =  tutorial_producer.GetTutorial();
-                        String finalUrl = url + "#" + tn.getUserObject();
+                        System.out.println(tn.getUserObject());
+                        String url = "";
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = null;
+                        Document document = null;
+                        try {
+                            builder = factory.newDocumentBuilder();
+                            InputStream is = Tutorial.class.getResourceAsStream("string.xml");
+                            document = builder.parse(new File(Tutorial.class.getResource("string.xml").getFile()));
+                            if (document == null)
+                                document = builder.parse(new File("main/resources/string.xml"));
+                        } catch (ParserConfigurationException | SAXException | IOException ex) {
+                            ex.printStackTrace();
+                        }
 
+                        assert document != null;
+
+                        NodeList nodeList = document.getElementsByTagName("url");
+                        Node node = nodeList.item(1);
+                        url = node.getNodeValue();
+//                        System.out.println(url);
+
+                        String finalUrl = url + "#" + tn.getUserObject();
+//                        System.out.println(finalUrl);
                         if (Desktop.isDesktopSupported()) {
                             try {
                                 Desktop desktop = Desktop.getDesktop();
