444e2dd0133f09dd6000ba33d11da42adc279201
candymate <parkjuny@postech.ac.kr>
2019-11-23 18:11:49 +0900

    Added 3 second delay on tree update (not working properly yet)

--- project/src/main/java/com/SoftwareMatrix/UpdateManager.java
+++ project/src/main/java/com/SoftwareMatrix/UpdateManager.java
@@ -1,5 +1,7 @@
 package com.SoftwareMatrix;
 
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -13,12 +15,69 @@ import java.util.List;
  */
 public class UpdateManager {
     private static UpdateManager instance = null;
+
+    private static Thread thread = null; // to track update time
+
     private Project project;
     private List<UpdateObserver> mObservers; // method observer list
     private List<UpdateObserver> cObservers; // class observer list
     private List<UpdateObserver> pObservers; // package observer list
 
-    private void notifyObservers(PsiTreeChangeEvent event) {
+
+    private void notifyObserversWrapper(PsiTreeChangeEvent event) {
+        System.out.println("CHANGED");
+        if(thread != null && thread.isAlive()) {
+            ; // do nothing
+        }
+        else {
+            thread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        ; // do nothing
+                    }
+                }
+            };
+            System.out.println("NOTIFY COMPLETE");
+            notifyObservers(event); // only allowed in main thread
+            thread.start();
+        }
+    }
+
+    private UpdateManager(@NotNull Project project) {
+        mObservers = new ArrayList<>();
+        cObservers = new ArrayList<>();
+        pObservers = new ArrayList<>();
+
+        this.project = project;
+        PsiManager.getInstance(project).addPsiTreeChangeListener(new PsiTreeChangeAdapter() {
+            @Override
+            public void childAdded(@NotNull PsiTreeChangeEvent event) {
+                notifyObserversWrapper(event);
+            }
+
+            @Override
+            public void childRemoved(@NotNull PsiTreeChangeEvent event) {
+                notifyObserversWrapper(event);
+            }
+
+            @Override
+            public void childReplaced(@NotNull PsiTreeChangeEvent event) {
+                notifyObserversWrapper(event);
+            }
+        });
+    }
+
+    public static UpdateManager getInstance(@NotNull Project project) {
+        if(instance == null) {
+            instance = new UpdateManager(project);
+        }
+        return instance;
+    }
+
+    public void notifyObservers(PsiTreeChangeEvent event) {
         for(PsiElement elem : List.of(event.getChild(), event.getParent())) {
             PsiPackage pElem = PsiTreeUtil.getParentOfType(elem, PsiPackage.class, false);
             PsiClass cElem = PsiTreeUtil.getParentOfType(elem, PsiClass.class, false);
@@ -47,37 +106,6 @@ public class UpdateManager {
                 }
             }
         }
-    }
-
-    private UpdateManager(@NotNull Project project) {
-        mObservers = new ArrayList<>();
-        cObservers = new ArrayList<>();
-        pObservers = new ArrayList<>();
-
-        this.project = project;
-        PsiManager.getInstance(project).addPsiTreeChangeListener(new PsiTreeChangeAdapter() {
-            @Override
-            public void childAdded(@NotNull PsiTreeChangeEvent event) {
-                notifyObservers(event);
-            }
-
-            @Override
-            public void childRemoved(@NotNull PsiTreeChangeEvent event) {
-                notifyObservers(event);
-            }
-
-            @Override
-            public void childReplaced(@NotNull PsiTreeChangeEvent event) {
-                notifyObservers(event);
-            }
-        });
-    }
-
-    public static UpdateManager getInstance(@NotNull Project project) {
-        if(instance == null) {
-            instance = new UpdateManager(project);
-        }
-        return instance;
     }
 
     public boolean addMethodObserver(UpdateObserver observer) {
