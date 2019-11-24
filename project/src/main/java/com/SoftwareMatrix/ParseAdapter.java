d2cba0ba136cfb12c378dd9a89efc6e5ccaad5ec
candymate <parkjuny@postech.ac.kr>
2019-11-24 18:19:03 +0900

    Added some basic PSI functions (30 min)

--- project/src/main/java/com/SoftwareMatrix/ParseAdapter.java
+++ project/src/main/java/com/SoftwareMatrix/ParseAdapter.java
@@ -1,5 +1,132 @@
 package com.SoftwareMatrix;
 
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/*
+    Contains functions related to parsing
+ */
 public class ParseAdapter {
+    /**
+     * Returns the containing method of element
+     *
+     * @param elem target element
+     *
+     * @return PsiMethod instance of containing method, or null if failed
+     */
+    public static PsiMethod getContainingMethod(@NotNull PsiElement elem) {
+        return PsiTreeUtil.getParentOfType(elem, PsiMethod.class, false);
+    }
+
+    /**
+     * Returns the containing class of element
+     *
+     * @param elem target element
+     *
+     * @return PsiClass instance of containing class, or null if failed
+     */
+    public static PsiClass getContainingClass(@NotNull PsiElement elem) {
+        return PsiTreeUtil.getParentOfType(elem, PsiClass.class, false);
+    }
+
+    /**
+     * Returns the containing package of element
+     *
+     * @param elem target element
+     *
+     * @return PsiPackage instance of containing package, or null if failed
+     */
+    public static PsiPackage getContainingPackage(@NotNull PsiElement elem) {
+        PsiPackage pElem = PsiTreeUtil.getParentOfType(elem, PsiPackage.class, false);
+        PsiClass cElem = getContainingClass(elem);
+        if(pElem == null && cElem != null) {
+            pElem = JavaDirectoryService.getInstance().getPackage(
+                    cElem.getContainingFile().getContainingDirectory());
+        }
+        return pElem;
+    }
+
+    /**
+     * Returns the list of instances of given class type
+     *  that exists in target element
+     *
+     * @param elem target element
+     * @param type target class type (e.g PsiIfStatement.class)
+     *
+     * @return list of instances of given class type
+     */
+    public static Collection<?> getTokensType(@NotNull PsiElement elem,
+                                              Class<? extends PsiElement> type) {
+        return Collections.unmodifiableCollection(
+                PsiTreeUtil.findChildrenOfType(elem, type));
+    }
+
+    /**
+     * Returns the list of instances of given class type
+     *  that exists in containing method of target element
+     *
+     * @param elem target element
+     * @param type target class type (e.g PsiIfStatement.class)
+     *
+     * @return list of instances of given class type
+     *         that exists in containing method of target element
+     *         empty list if containing method does not exist
+     */
+    public static Collection<?> getTokensTypeMethod(@NotNull PsiElement elem,
+                                                    Class<? extends PsiElement> type) {
+        PsiMethod m = getContainingMethod(elem);
+        if(m == null)
+            return Collections.unmodifiableCollection(Collections.emptyList());
+
+        return Collections.unmodifiableCollection(
+                PsiTreeUtil.findChildrenOfType(m, type));
+    }
+
+    /**
+     * Returns the list of instances of given class type
+     *  that exists in containing class of target element
+     *
+     * @param elem target element
+     * @param type target class type (e.g PsiIfStatement.class)
+     *
+     * @return list of instances of given class type
+     *         that exists in containing class of target element
+     *         empty list if containing class does not exist
+     */
+    public static Collection<?> getTokensTypeClass(@NotNull PsiElement elem,
+                                                   Class<? extends PsiElement> type) {
+        PsiClass c = getContainingClass(elem);
+        if(c == null)
+            return Collections.unmodifiableCollection(Collections.emptyList());
+
+        return Collections.unmodifiableCollection(
+                PsiTreeUtil.findChildrenOfType(c, type));
+    }
+
+    /**
+     * Returns the list of instances of given class type
+     *  that exists in containing method of target element
+     *
+     * @param elem target element
+     * @param type target class type (e.g PsiIfStatement.class)
+     *
+     * @return list of instances of given class type
+     *         that exists in containing package of target element
+     *         empty list if containing package does not exist
+     */
+    public static Collection<?> getTokensTypePackage(@NotNull PsiElement elem,
+                                                     Class<? extends PsiElement> type) {
+        PsiPackage p = getContainingPackage(elem);
+        if(p == null)
+            return Collections.unmodifiableCollection(Collections.emptyList());
+
+        return Collections.unmodifiableCollection(
+                PsiTreeUtil.findChildrenOfType(p, type));
+    }
+
 
 }
